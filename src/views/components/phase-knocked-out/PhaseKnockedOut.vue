021753242c970cfe2d78608a14103d93aba9c03d
Constantinos Stathis <constantinos-@hotmail.com>
2023-08-06 15:36:54 +0300

    Fixed defeating description

--- src/views/components/phase-knocked-out/PhaseKnockedOut.vue
+++ src/views/components/phase-knocked-out/PhaseKnockedOut.vue
@@ -7,7 +7,8 @@ import PhaseTitle from '../phase-title/PhaseTitle.vue';
     <div>
       <ol>
         <li>Take 1 Trail Token matching monsters Terrain (if you already haven't one).</li>
-        <li>Add 0-cost Action card into your hand</li>
+        <li>Add 0-cost Action card into your hand.</li>
+        <li>In phase III draw up to 2 cards.</li>
       </ol>
     </div>
   </div>
