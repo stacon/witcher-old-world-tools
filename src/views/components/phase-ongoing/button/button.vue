ce8471092d54ef7ffbb1b8ea71d90c304b02d158
Constantinos Stathis <constantinos-@hotmail.com>
2023-08-19 14:43:58 +0300

    Fixed disabled button click

--- src/views/components/phase-ongoing/button/button.vue
+++ src/views/components/phase-ongoing/button/button.vue
@@ -11,8 +11,10 @@ const emit = defineEmits(['click']);
   <button
     :[disabled]="disabled"
     class="px-2 border rounded-lg text-black border-black hover:bg-gray-100"
-    :class="{ 'border-gray-300 text-gray-300 italic hover:bg-transparent': disabled }"
-    @click="emit('click')"
+    :class="{
+      'border-gray-300 text-gray-300 italic hover:bg-transparent cursor-default': disabled,
+    }"
+    @click="!disabled && emit('click')"
   >
     <slot></slot>
   </button>
